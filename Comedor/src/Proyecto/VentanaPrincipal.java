c10614f9cebc6258c69052792a1048f32110228d
goyos12345 <146874891+goyos12345@users.noreply.github.com>
2026-09-05 17:50:03 -0300

    Add files via upload

--- Comedor/src/Proyecto/VentanaPrincipal.java
+++ Comedor/src/Proyecto/VentanaPrincipal.java
@@ -1,6 +1,7 @@
 package Proyecto;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -38,7 +39,6 @@ public class VentanaPrincipal extends JFrame {
 		JLabel NadaLab = new JLabel("Seleccione una de las opciones de arriba");
 
 		// Creación de elementos del panel inferior
-		
 
 		this.add(panSup, BorderLayout.NORTH);
 		panSup.add(busTxt);
@@ -54,35 +54,61 @@ public class VentanaPrincipal extends JFrame {
 		granPan.add(NadaLab);
 
 		this.add(panInf, BorderLayout.SOUTH);
-		
+		panInf.setLayout(new FlowLayout(FlowLayout.LEFT));
 
-		//Funciones de los botones de arriba
+		// Funciones de los botones de arriba
 		ingBut.addActionListener(new ActionListener(
-				
-				) {
-			
+
+		) {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				granPan.removeAll();
+				granPan.setLayout(new FlowLayout(FlowLayout.LEFT));
 				System.out.println("Anda");
-				//Remover la nada
-				granPan.remove(NadaLab);
-				//Creación de los paneles del medio
-				JPanel panIngDerecha=new JPanel();
-				JPanel panIngIzquierda=new JPanel();
-				//Los PlaceHolders hay que borrarlos
-				JButton PlaceHolder1=new JButton("PlaceHolder");
-				JButton PlaceHolder2=new JButton("PlaceHolder");
-				JButton PlaceHolder3=new JButton("PlaceHolder");
-				JButton PlaceHolder4=new JButton("PlaceHolder");
+
+				// Creación de los paneles del medio
+				JPanel panIngDerecha = new JPanel();
+				JPanel panIngIzquierda = new JPanel();
+
+				// Los PlaceHolders hay que borrarlos
+				JButton PlaceHolder1 = new JButton("PlaceHolder");
+				JButton PlaceHolder2 = new JButton("PlaceHolder");
+				JButton PlaceHolder3 = new JButton("PlaceHolder");
+				JButton PlaceHolder4 = new JButton("PlaceHolder");
+
+				// Creación del srcoreaas
+				
+
+				// Tabla
+				String[] secciones = { "ID", "Nombres", "Días", "Limitaciónes aptas", "Platos", "Horario" };
+				DefaultTableModel modelo = new DefaultTableModel(secciones, 40);
+				modelo.addRow(new Object[] { "1", "Menú 1", "Lunes", "Intolerante al gluten", "Milanesa", "17:30" });
+
+				JTable tablita = new JTable(modelo);
+				JScrollPane scroll = new JScrollPane(tablita);
+				
+				
+				//Panel izquierdo
 				granPan.add(panIngIzquierda);
+				PlaceHolder1.setPreferredSize(new Dimension(100, 100));
 				panIngIzquierda.add(PlaceHolder1);
 				panIngIzquierda.add(PlaceHolder2);
 				panIngIzquierda.add(PlaceHolder3);
 				panIngIzquierda.add(PlaceHolder4);
+
+				panIngIzquierda.setLayout(new BoxLayout(panIngIzquierda, BoxLayout.Y_AXIS));
+
+				panIngIzquierda.repaint();
+				panIngIzquierda.revalidate();
 				
-				
-				
-				//cosos de los paneles de abajo
+				//Panel de la derecha
+				granPan.add(panIngDerecha);
+				panIngDerecha.add(scroll);
+				scroll.setPreferredSize(new Dimension(1150,700));
+
+				// cosos de los paneles de abajo
+				panInf.removeAll();
 				JButton adminIngBut = new JButton("+");
 				JLabel adminIngLab = new JLabel("Administrar ingredientes");
 				JButton adminGruIngBut = new JButton("+");
@@ -92,6 +118,11 @@ public class VentanaPrincipal extends JFrame {
 				panInf.add(adminGruIngBut);
 				panInf.add(admGruIngLab);
 				
+				
+				panInf.repaint();
+
+				granPan.repaint();
+
 			}
 		});
 
